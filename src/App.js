1b1415631b890670f96affae80e31b8173bc621e
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2022-08-12 11:23:29 +0200

    prettier fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Banner from './components/Banner/Banner';
 import Welcome from './Pages/Welcome';
-import Button from './components/Button/Button';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Registration from './Pages/Registration'
+// import Banner from './components/Banner/Banner';
+// import Button from './components/Button/Button';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Registration from './Pages/Registration'
 
 function App() {
   return (
