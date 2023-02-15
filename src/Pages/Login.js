0ab43288fc1aac24bb94f87d7d73415852511155
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2023-02-15 18:18:48 +0100

    login auth and redirection

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,17 +2,53 @@
 import React, { useState } from 'react';
 import './Login.css';
 import { Button } from '../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  const [mail, setMail] = useState('');
-  const [mdp, setMdp] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+  const [auth, setAuth] = useState(Boolean);
 
-  const handleMail = (e) => {
-    setMail(e.target.value);
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
   };
 
-  const handleMdp = (e) => {
-    setMdp(e.target.value);
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const login = () => {
+    fetch('http://188.165.238.74:8080/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(token);
+        setToken(data);
+        setAuth(true);
+      })
+      .catch((err) => {
+        console.error(err);
+        setAuth(false);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login();
+    if (auth === true) {
+      console.log('CONNECTED');
+      navigate('/Edit');
+    }
   };
 
   return (
@@ -26,16 +62,16 @@ export default function Login() {
             <div className="login-form">
               <form>
                 <input
-                  onChange={handleMail}
+                  onChange={handleEmail}
                   className="input"
-                  value={mail}
+                  value={email}
                   type="text"
                   placeholder="Mail"
                 />
                 <input
-                  onChange={handleMdp}
+                  onChange={handlePassword}
                   className="input"
-                  value={mdp}
+                  value={password}
                   type="text"
                   placeholder="Mot de passe"
                 />
@@ -49,9 +85,7 @@ export default function Login() {
             </p>
             <a href="/Login">
               <Button
-                onClick={() => {
-                  console.log('working');
-                }}
+                onClick={handleSubmit}
                 type="button"
                 buttonStyle="btn--primary--solid"
                 buttonSize="btn--medium">
